0f4ffae1424fe28730df381e521475385cccbc4d
Brandon Williams <brandonawilliams1@gmail.com>
2021-10-24 23:11:42 -0500

    some JS added some functions are working but progress bar isn't updating

--- game.js
+++ game.js
@@ -15,15 +15,15 @@
 
 const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
-const progressText = document.querySelector('#progressText');
-const scoreText = document.querySelector('#score');
+const progressText = document.querySelector('#progressText');;
+const scoreText = document.querySelector('#score')
 const progressBarFull = document.querySelector('#progressBarFull');
 
-let currentQuestion = {};
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions= [];
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
 
 let questions = [
     {
@@ -32,7 +32,7 @@ let questions = [
         choice2: "Hyper Text Markup Landing",
         choice3: "Hyper Text Markup Language",
         choice4: "Heavy text Main Line",
-        answer: "Hyper Text Markup Language",
+        answer: 3,
     },
     {
         question: "What does the acronym CSS stand for?",
@@ -40,7 +40,7 @@ let questions = [
         choice2: "Cascading Style Sheet",
         choice3: "Crime Style Savage",
         choice4: "Clean Simp System",
-        answer: "Cascading Style Sheet",
+        answer: 2,
     },
     {
         question: "What is the data type for the numeral 12 ?",
@@ -48,7 +48,7 @@ let questions = [
         choice2: "Null",
         choice3: "sting",
         choice4: "Number",
-        answer: "Number",
+        answer: 4,
     },
     {
         question: "What does the acronym \"HTML\" stand for?",
@@ -56,30 +56,32 @@ let questions = [
         choice2: "Hyper Text Markup Landing",
         choice3: "Hyper Text Markup Language",
         choice4: "Heavy text Main Line",
-        answer: "what does the acronym DOM stand for?",
+        answer: 3,
     },
 ]
 
-const scorePoints = 100;
-const maxQuestions = 4;
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
 
 startGame = () => {
-    questionCounter = 0;
-    score = 0;
+    questionCounter = 0
+    score = 0
     availableQuestions = [...questions]
     getNewQuestion()
 }
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > maxQuestions) {
-    localStorage.setItem("mostRecentScore", score)
-    return window.location.assign("/end.html")
-    }
-    questionCounter++
-    progressText.innerText = `Question ${questionCounter} of maxQuestions`
-    progressBarFull.style.width = `${(questionCounter/maxQuestions)* 100}%`
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    localStorage.setItem('mostRecentScore', score)
     
-    const questionsIndex = math.floor(math.random() * availableQuestions.lenght)
+    return window.location.assign('/end.html')
+    }
+    
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionsIndex]
     question.innerText = currentQuestion.question
 
@@ -89,4 +91,36 @@ getNewQuestion = () => {
     })
 
     availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
 }
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+        
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score += num
+    scoreText.innerText = score
+}
+
+startGame()
